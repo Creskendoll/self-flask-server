39741d0370aa582d572b74bdebfc12dcc4ec2661
Kenan Soylu <ken.soylu@hotmail.com>
2019-05-13 16:19:47 +0300

    pokemoned v1

---
+++ static/js/pokemoned/precache-manifest.b89657c5a9124443ded92520dadfd96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef74ca67694ad7439e27c47c12327d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa92cf812b653ebd310",
+    "url": "/static/css/main.67d19e07.chunk.css"
+  },
+  {
+    "revision": "d2b6e224a8e4f8db5b27",
+    "url": "/static/js/2.738b7bb0.chunk.js"
+  },
+  {
+    "revision": "1aa92cf812b653ebd310",
+    "url": "/static/js/main.6cf5b68e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "48e2f87d20bc71e859f9f578876b4a46",
+    "url": "/static/media/loading.48e2f87d.gif"
+  }
+]);
