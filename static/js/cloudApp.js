4b9bf79631d56a4740d4aeddd8ea64758f3fc2a4
Kenan Soylu <ken.soylu@hotmail.com>
2019-05-14 16:16:53 +0300

    cloud apps, resume

--- static/js/cloudApp.js
+++ static/js/cloudApp.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($){
     const leftArrow = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-left-b-512.png";
     const downArrow = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-arrow-down-b-512.png";
 
-    $(".question-head").click(function() {
+    $(".question-container").click(function() {
         let arrowImg = $(this).find(".expand-arrow-image");
         const current = arrowImg.attr("src");
         arrowImg.attr("src", current === leftArrow ? downArrow : leftArrow);
