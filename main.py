d9db369a19d115767c8d0227630a0ed69cb849b5
Kenan Soylu <ken.soylu@hotmail.com>
2020-02-25 15:24:54 +0300

    update resume

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 # Imports
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 
 from flask import Flask, request, Response
 import logging
@@ -10,32 +10,32 @@ import requests
 from flask_cors import cross_origin
 from flask_mail import Mail, Message
 import json
-#----------------------------------------------------------------------------#
-# App Config.
-#----------------------------------------------------------------------------#
-app = Flask(__name__, static_folder='static', static_url_path='')
-app.config.from_object('config')
 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+# App Config.
+# ----------------------------------------------------------------------------#
+app = Flask(__name__, static_folder="static", static_url_path="")
+app.config.from_object("config")
+
+# ----------------------------------------------------------------------------#
 # Mail.
-#----------------------------------------------------------------------------#
-app.config['MAIL_SERVER'] = 'smtp.live.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+# ----------------------------------------------------------------------------#
+app.config["MAIL_SERVER"] = "smtp.live.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
 # app.config['MAIL_USE_SSL'] = True
 
-app.config['MAIL_USERNAME'] =  'ken.soylu@hotmail.com' # enter your name here
-app.config['MAIL_DEFAULT_SENDER'] = 'ken.soylu@hotmail.com' # enter your email here
-app.config['MAIL_PASSWORD'] = '3hardcore1' # enter your password here
+app.config["MAIL_USERNAME"] = "ken.soylu@hotmail.com"  # enter your name here
+app.config["MAIL_DEFAULT_SENDER"] = "ken.soylu@hotmail.com"  # enter your email here
+app.config["MAIL_PASSWORD"] = "3hardcore1"  # enter your password here
 
 mail = Mail(app)
 
 PROXY_URL = "http://34.89.189.41:5001"
 # PROXY_URL = "http://localhost:5001"
-# PROXY_URL = "http://192.168.0.55:5001"
 
 # Login required decorator.
-'''
+"""
 def login_required(test):
     @wraps(test)
     def wrap(*args, **kwargs):
@@ -45,46 +45,60 @@ def login_required(test):
             flash('You need to login first.')
             return redirect(url_for('login'))
     return wrap
-'''
-#----------------------------------------------------------------------------#
+"""
+# ----------------------------------------------------------------------------#
 # Controllers.
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+
 
 @app.route("/cloud-applications")
 def hw():
     return app.send_static_file("cloudApp.html")
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return app.send_static_file("index.html")
+
 
 def proxy(redirect_url):
 
     resp = requests.request(
         method=request.method,
         url=redirect_url,
-        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        headers={key: value for (key, value) in request.headers if key != "Host"},
         data=request.get_data(),
         cookies=request.cookies,
-        allow_redirects=False)
+        allow_redirects=False,
+    )
 
-    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in resp.raw.headers.items()
-               if name.lower() not in excluded_headers]
+    excluded_headers = [
+        "content-encoding",
+        "content-length",
+        "transfer-encoding",
+        "connection",
+    ]
+    headers = [
+        (name, value)
+        for (name, value) in resp.raw.headers.items()
+        if name.lower() not in excluded_headers
+    ]
 
     response = Response(resp.content, resp.status_code, headers)
     return response
 
-@cross_origin(headers=['Content-Type'])
-@app.route('/vm/<path:path>', methods=['POST', 'GET'])
+
+@cross_origin(headers=["Content-Type"])
+@app.route("/vm/<path:path>", methods=["POST", "GET"])
 def vm(path):
 
-    redirect_url = request.url.replace(request.host_url+"vm", PROXY_URL)
+    redirect_url = request.url.replace(request.host_url + "vm", PROXY_URL)
 
     return proxy(redirect_url)
 
-@app.route('/pokemoned', methods=["GET"])
-@cross_origin(headers=['Content-Type'])
+
+@app.route("/pokemoned", methods=["GET"])
+@cross_origin(headers=["Content-Type"])
 def pokikiGET():
     if request.args.get("image") is not None:
 
@@ -92,42 +106,42 @@ def pokikiGET():
 
         return proxy(redirect_url)
     else:
-        return app.send_static_file('pokemoned.html')
-        
-@app.route('/pokemoned/post-image', methods=["POST"])
-@cross_origin(headers=['Content-Type'])
+        return app.send_static_file("pokemoned.html")
+
+
+@app.route("/pokemoned/post-image", methods=["POST"])
+@cross_origin(headers=["Content-Type"])
 def postImg():
     redirect_url = PROXY_URL + "/pokemoned/post-image"
     return proxy(redirect_url)
-        
-@app.route('/send-mail', methods=['POST'])
-@cross_origin(headers=['Content-Type'])
-def send_mail():
-    # DEBUG
-    # return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
+
+@app.route("/send-mail", methods=["POST"])
+@cross_origin(headers=["Content-Type"])
+def send_mail():
     sender_name = request.form["sender_name"]
     sender_mail = request.form["sender_mail"]
     subject = request.form["mail_subject"]
     body = request.form["mail_body"]
     # print("Received: {} {} {} {}".format(sender_name, sender_mail, subject, body))
-    
+
     # Send email to self
-    msg = Message("Portfolio Site: " + subject, recipients=['ken.soylu@hotmail.com'])
+    msg = Message("Portfolio Site: " + subject, recipients=["ken.soylu@hotmail.com"])
     msg.body = "{} \n From: {} - {}".format(body, sender_name, sender_mail)
     mail.send(msg)
-    
+
     # Send another mail to sender
     thanks_msg = Message("Thanks for your message!", recipients=[sender_mail])
-    thanks_msg.body = "This is a confirmation that I have recieved your mail and will be responding shortly.\nSincerely,\nKenan Soylu"
+    thanks_msg.body = f"Hey there {sender_name},\n\nThis is a confirmation that I have recieved your mail and will be responding shortly.\n\nSincerely,\n# Kenan Soylu"
     mail.send(thanks_msg)
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
 
 
 # Error handlers.
 @app.errorhandler(404)
 def not_found_error(error):
     return app.send_static_file("404.html"), 404
+
 
 # if not app.debug:
 #     file_handler = FileHandler('error.log')
@@ -139,12 +153,12 @@ def not_found_error(error):
 #     app.logger.addHandler(file_handler)
 #     app.logger.info('errors')
 
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 # Launch.
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.config["ENVIRONMENT"] = "development"
-    app.run(host='0.0.0.0', port=port)
-    
+    app.run(host="0.0.0.0", port=port)
+
--- static/js/custom.js
+++ static/js/custom.js
@@ -13,53 +13,49 @@
 
 //=====================================================================================
 
-(function ($) {
+(function($) {
   "use strict";
 
   var $body = $("body"),
-    $window = $(window)
+    $window = $(window);
 
   $body.scrollspy({
     target: ".navbar-collapse",
     offset: 20
-
   });
-
 
   /*=========================================================================== 
     // 00.   Navbar extra
     ============================================================================*/
-  $('button.navbar-toggler').click(() => {
-    let addClass = 'nav-expanded'
+  $("button.navbar-toggler").click(() => {
+    let addClass = "nav-expanded";
     let navbar = $(".main_nav");
-    if (navbar.hasClass(addClass))
-      navbar.removeClass(addClass);
-    else
-      navbar.addClass(addClass);
+    if (navbar.hasClass(addClass)) navbar.removeClass(addClass);
+    else navbar.addClass(addClass);
   });
 
-  $('.blog_item').click(function () {
-    var win = window.open($(this).attr('link'), '_blank');
+  $(".blog_item").click(function() {
+    var win = window.open($(this).attr("link"), "_blank");
     if (win) {
       //Browser has allowed it to be opened
       win.focus();
     } else {
       //Browser has blocked it
-      alert('Please allow popups for this website.');
+      alert("Please allow popups for this website.");
     }
   });
-  $('body').click(() => {
-    $('.navbar-collapse').collapse('hide');
+  $("body").click(() => {
+    $(".navbar-collapse").collapse("hide");
   });
 
   /*=========================================================================== 
     // 01.   Typed Text
   ============================================================================*/
 
-  $(".element").each(function () {
+  $(".element").each(function() {
     var $this = $(this);
     $this.typed({
-      strings: $this.attr('data-elements').split(','),
+      strings: $this.attr("data-elements").split(","),
       typeSpeed: 100,
       backDelay: 3000
     });
@@ -69,18 +65,14 @@
   // 02.    Navbar scrolling logo change
   ===========================================================================*/
 
-  $window.on("scroll", function () {
-
+  $window.on("scroll", function() {
     var bodyScroll = $window.scrollTop(),
       navbar = $(".main_nav"),
       logo = $(".main_nav .navbar-brand> img");
 
     if (bodyScroll > 100) {
-
       navbar.addClass("nav-scroll");
-
     } else {
-
       navbar.removeClass("nav-scroll");
     }
   });
@@ -89,19 +81,21 @@
   // 03.    Smoothscroll js
   =======================================================================*/
 
-  $("a").on('click', function (event) {
-
+  $("a").on("click", function(event) {
     if (this.hash !== "") {
       event.preventDefault();
 
       var hash = this.hash;
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function () {
-
-        window.location.hash = hash;
-      });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1000,
+        function() {
+          window.location.hash = hash;
+        }
+      );
     }
   });
 
@@ -109,16 +103,18 @@
   //  04.   Testimonial Slider
   //=====================================================================================
 
-  $('.testimonial_item').owlCarousel({
+  $(".testimonial_item").owlCarousel({
     loop: true,
     autoplay: true,
     autoplayTimeout: 5000,
     margin: 0,
     nav: true,
     dots: false,
-    navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
+    navText: [
+      '<span class="fa fa-angle-left"></span>',
+      '<span class="fa fa-angle-right"></span>'
+    ],
     responsive: {
-
       0: {
         items: 1
       },
@@ -132,7 +128,6 @@
         items: 2
       }
     }
-
   });
 
   //=====================================================================================
@@ -144,14 +139,13 @@
     var bottomview = topview + $(window).height();
     var topelement = $elementchecked.offset().top;
     var bottomelement = topelement + $elementchecked.height();
-    return ((bottomelement <= bottomview) && (topelement >= topview));
+    return bottomelement <= bottomview && topelement >= topview;
   }
 
   function factCounter() {
-    $('.fact-counter .count').each(function () {
+    $(".fact-counter .count").each(function() {
       var $t = $(this);
       if (iscountervisible($t)) {
-
         var n = $t.find(".count-num").attr("data-stop"),
           r = parseInt($t.find(".count-num").attr("data-speed"), 10);
 
@@ -159,21 +153,24 @@
           $t.addClass("counted");
           $({
             countNum: $t.find(".count-num").text()
-          }).animate({
-            countNum: n
-          }, {
-            duration: r,
-            easing: "linear",
-            step: function () {
-              $t.find(".count-num").text(Math.floor(this.countNum));
+          }).animate(
+            {
+              countNum: n
             },
-            complete: function () {
-              $t.find(".count-num").text(this.countNum);
+            {
+              duration: r,
+              easing: "linear",
+              step: function() {
+                $t.find(".count-num").text(Math.floor(this.countNum));
+              },
+              complete: function() {
+                $t.find(".count-num").text(this.countNum);
+              }
             }
-          });
+          );
           //set skill building height
-          var size = $t.children('.progress-bar').attr('aria-valuenow');
-          $t.children('.progress-bar').css('width', size + '%');
+          var size = $t.children(".progress-bar").attr("aria-valuenow");
+          $t.children(".progress-bar").css("width", size + "%");
         }
       }
     });
@@ -183,10 +180,10 @@
   // 06.    Elements Animation
   //=====================================================================================
 
-  if ($('.wow').length) {
+  if ($(".wow").length) {
     var wow = new WOW({
-      boxClass: 'wow', // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
+      boxClass: "wow", // animated element css class (default is wow)
+      animateClass: "animated", // animation css class (default is animated)
       offset: 0, // distance to the element when triggering the animation (default is 0)
       mobile: false, // trigger animations on mobile devices (default is true)
       live: true // act on asynchronously loaded content (default is true)
@@ -194,15 +191,14 @@
     wow.init();
   }
 
-
   //=====================================================================================
   // 07.    When document is Scrollig
   //=====================================================================================
 
-  $window.on('scroll touchmove', function () {
+  $window.on("scroll touchmove", function() {
     factCounter();
   });
-  //   $('body').bind('touchmove', function(e) { 
+  //   $('body').bind('touchmove', function(e) {
   //     console.log("move");
   //     factCounter();
   // });
@@ -214,26 +210,25 @@
   $('[data-fancybox="gallery"]').fancybox({
     animationEffect: "zoom-in-out",
     transitionEffect: "slide",
-    transitionEffect: "slide",
+    transitionEffect: "slide"
   });
 
   //=====================================================================================
   //  09.   Gallery With Filters List
   //=====================================================================================
 
-  if ($('.filter-list').length) {
-    $('.filter-list').mixItUp({});
+  if ($(".filter-list").length) {
+    $(".filter-list").mixItUp({});
   }
 
   //=====================================================================================
   //  10.   Youtube and Vimeo video popup control
   //=====================================================================================
 
-  jQuery(function () {
+  jQuery(function() {
     jQuery("a.video-popup").YouTubePopUp();
     // jQuery("a.video-popup").YouTubePopUp( { autoplay: 1 } ); // Enable autoplay
   });
-
 })(jQuery);
 
 //=====================================================================================
@@ -245,38 +240,31 @@ const interval = 3000;
 
 function cycleImages() {
   let img_name = "url('images/background/" + img_index.toString() + ".jpg')";
-  $('#main_banner').css('background-image', img_name)
-  $('#main_banner').css({
+  $("#main_banner").css("background-image", img_name);
+  $("#main_banner").css({
     "background-size": "cover",
-    "position": "relative"
+    position: "relative"
   });
-  
-  if(img_index == 2) {
-    $("#main_banner").css({
-      "-webkit-transition": "background-image 1s ease-in-out",
-      "-moz-transition": "background-image 1s ease-in-out",
-      "-o-transition": "background-image 1s ease-in-out",
-      "transition": "background-image 1s ease-in-out",
-    });
-  }
+
   img_index = (img_index + 1) % N;
-};
+}
 function startCycle() {
-  setInterval('cycleImages()', interval);
+  setInterval("cycleImages()", interval);
 }
 let loaded_count = 0;
-Array.apply(null, {length: N}).map(Number.call, Number).forEach(i => {
-  let img_path = 'images/background/' + i.toString() + '.jpg';
-  let downloadingImage = new Image();
-  downloadingImage.onload = function () {
-    loaded_count++;
-    if (loaded_count == N) {
-      startCycle();
-    }
-  };
-  downloadingImage.src = img_path;
-});
-
+Array.apply(null, { length: N })
+  .map(Number.call, Number)
+  .forEach(i => {
+    let img_path = "images/background/" + i.toString() + ".jpg";
+    let downloadingImage = new Image();
+    downloadingImage.onload = function() {
+      loaded_count++;
+      if (loaded_count == N) {
+        startCycle();
+      }
+    };
+    downloadingImage.src = img_path;
+  });
 
 //=====================================================================================
 //  11.   MAIL
@@ -289,27 +277,25 @@ Array.apply(null, {length: N}).map(Number.call, Number).forEach(i => {
 // });
 
 $("#send").click(() => {
-
   $("#mail-loading").css("display", "");
 
   const data = {
-    "sender_name" : $("#mailName").val(),
-    "sender_mail" : $("#senderMail").val(),
-    "mail_subject" : $("#mailSubject").val(),
-    "mail_body" : $("#mailMessage").val()
-  }
+    sender_name: $("#mailName").val(),
+    sender_mail: $("#senderMail").val(),
+    mail_subject: $("#mailSubject").val(),
+    mail_body: $("#mailMessage").val()
+  };
 
   $.ajax({
     type: "POST",
     url: "/send-mail",
     data: data,
-    success: function (msg, status, jqXHR) {
-      alert("Thanks for your message! I'll try to respond ASAP!")
+    success: function(msg, status, jqXHR) {
+      alert("Thanks for your message! I'll try to respond ASAP!");
       $("#mail-loading").css("display", "none");
     },
-    error: function (jqXHR, textStatus, errorThrown)
-    {
-      alert("Couldn't send message... Sry :(")
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert("Couldn't send message... Sry :(");
       $("#mail-loading").css("display", "none");
     },
     dataType: "json"
@@ -319,46 +305,63 @@ $("#send").click(() => {
 //=====================================================================================
 //  12.   Carousel
 //=====================================================================================
-$('.carousel-control-prev').click(() => {
-  $("#carouselExampleControls").carousel('prev');
+$(".carousel-control-prev").click(() => {
+  $("#carouselExampleControls").carousel("prev");
 });
-$('.carousel-control-next').click(() => {
-  $("#carouselExampleControls").carousel('next');
+$(".carousel-control-next").click(() => {
+  $("#carouselExampleControls").carousel("next");
 });
 
 $("#carouselExampleControls").carousel({
-  interval : 3000
+  interval: 3000
 });
 const PHOTO_RANGE = 24;
 const PHOTO_COUNT_SECTION = 6;
 
 let caro_items = [];
 
-$(".carousel-img-container").each(function (carousel_item_index, _) {
-  let photo_items = Array(PHOTO_COUNT_SECTION).fill().map((_, i) => {
-    let img_index = ((carousel_item_index*PHOTO_COUNT_SECTION) + i);
-    if (img_index < PHOTO_RANGE) {
-      let img_name = "images/portfolio/gallery/"+ img_index.toString()+".jpg";
-      let a_href =  "images/portfolio/gallery/"+ img_index.toString()+".jpg";
-      
-      let photo_item = $(".photo-item").clone();
-      photo_item.attr("hidden", false);
-      let downloadingImage = new Image();
-      downloadingImage.onload = function () {
-        $(".photo-item").eq(0).find("a").attr("data-fancybox", "");
-        photo_item.find("a").attr("href", a_href);
-        photo_item.find("img").attr("src", img_name);
-      };
-      downloadingImage.src = img_name;
-      return photo_item;
-    }
-  });
-  // $(".carousel-img-container").eq(carousel_item_index).append(photo_items);  
-    caro_items.push(photo_items);
+$(".carousel-img-container").each(function(carousel_item_index, _) {
+  let photo_items = Array(PHOTO_COUNT_SECTION)
+    .fill()
+    .map((_, i) => {
+      let img_index = carousel_item_index * PHOTO_COUNT_SECTION + i;
+      if (img_index < PHOTO_RANGE) {
+        let img_name =
+          "images/portfolio/gallery/" + img_index.toString() + ".jpg";
+        let a_href =
+          "images/portfolio/gallery/" + img_index.toString() + ".jpg";
+
+        let photo_item = $(".photo-item").clone();
+        photo_item.attr("hidden", false);
+        let downloadingImage = new Image();
+        downloadingImage.onload = function() {
+          $(".photo-item")
+            .eq(0)
+            .find("a")
+            .attr("data-fancybox", "");
+          photo_item.find("a").attr("href", a_href);
+          photo_item.find("img").attr("src", img_name);
+        };
+        downloadingImage.src = img_name;
+        return photo_item;
+      }
+    });
+  // $(".carousel-img-container").eq(carousel_item_index).append(photo_items);
+  caro_items.push(photo_items);
 });
 
-$(".carousel-img-container").eq(0).append(caro_items[0]);
-$(".carousel-img-container").eq(1).append(caro_items[1]);
-$(".carousel-img-container").eq(2).append(caro_items[2]);
-$(".carousel-img-container").eq(3).append(caro_items[3]);
-$(".carousel-img-container").eq(4).append(caro_items[4]);
+$(".carousel-img-container")
+  .eq(0)
+  .append(caro_items[0]);
+$(".carousel-img-container")
+  .eq(1)
+  .append(caro_items[1]);
+$(".carousel-img-container")
+  .eq(2)
+  .append(caro_items[2]);
+$(".carousel-img-container")
+  .eq(3)
+  .append(caro_items[3]);
+$(".carousel-img-container")
+  .eq(4)
+  .append(caro_items[4]);
